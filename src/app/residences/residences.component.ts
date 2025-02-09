e380d8f532ff7eb6852b1dad40727988d908691b
chaima2128 <chaima.khanfir@esprit.tn>
2025-02-09 16:08:55 +0100

    Premier commit

---
+++ src/app/residences/residences.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Residence } from '../core/models/Residence';
+
+@Component({
+  selector: 'app-residences',
+  templateUrl: './residences.component.html',
+  styleUrls: ['./residences.component.css']
+})
+export class ResidencesComponent {
+  listResidences:Residence[]=[
+    {id:1,"name": "El fel","address":"Borj Cedria", "image":"../../assets/images/R1.jfif", status: "Disponible"},
+     {id:2,"name": "El yasmine", "address":"Ezzahra","image":"../../assets/images/R2.jfif", status: "Disponible" },
+     {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jfif", status: "Vendu"},
+     {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jfif", status: "En Construction"}
+   ];
+   favoriteResidences: Residence[] = [];
+  searchTerm: string = '';
+
+  showLocation(residence: Residence) {
+    if (residence.address === "inconnu") {
+      alert("L'adresse de cette résidence est inconnue");
+    } else {
+      alert(`Adresse: ${residence.address}`);
+    }
+  }
+
+  addToFavorites(residence: Residence) {
+    if (!this.favoriteResidences.includes(residence)) {
+      this.favoriteResidences.push(residence);
+    }
+  }
+
+  filterResidences(): Residence[] {
+    return this.listResidences.filter(residence =>
+      residence.address.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+}
